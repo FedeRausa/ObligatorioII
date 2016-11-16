39312495dc9ea679ed8530c95a1c07f95a0a5c93
Federico <Federico@Fede>
2016-11-16 20:15:26 -0200

    Se crean las clases del proyecto, sus atributos, constructores, geters y seters.

--- Obligatorio2/src/obligatorio2/Obligatorio2.java
+++ Obligatorio2/src/obligatorio2/Obligatorio2.java
@@ -5,20 +5,28 @@
  */
 package obligatorio2;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Federico
  */
 public class Obligatorio2 {
+    
+    private ArrayList<Evaluacion> listaFichas;
+    private ArrayList<Usuario> listaUsuarios;
+    private ArrayList<Usuario> listaUsuariosConFicha;
+    private ArrayList<Sorteo> listaSorteos;
+    private ArrayList<Restoran> listaRestoranes;
+    private ArrayList<Evaluacion> listaEvaluaciones;
+    private ArrayList<String> listaComidas;
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-   System.out.println("Hola Mundo");
-   
-   
-   //****jbhvbhjghj***///
+        // TODO code application logic here
+        
     }
     
 }
